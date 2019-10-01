b1e80791a8145e4ae6973194f3bdf6a0f0ff1406
eethan1 <call132call132@gmail.com>
2019-10-01 16:52:45 +0800

    add NTUAutoLogin.js

---
+++ NTULAutoLogin.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         New ES5-Userscript
+// @namespace    https://github.com/eethan1/TampermonkeyScripts
+// @require http://code.jquery.com/jquery-latest.js
+// @updateURL    https://raw.githubusercontent.com/eethan1/TampermonkeyScripts/master/NTULAutoLogin.js
+// @downloadURL  https://raw.githubusercontent.com/eethan1/TampermonkeyScripts/master/NTULAutoLogin.js
+// @version      0.1
+// @description  auto login
+// @author       eethan1
+// @match        https://web2.cc.ntu.edu.tw/p/s/login2/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    $('form').submit();
+    // Your code here...
+})();
