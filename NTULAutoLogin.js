36a542c9726f093f7e67cb4386da549339ed6679
eethan1 <call132call132@gmail.com>
2019-12-13 15:03:17 +0800

    upgrade NTUAutoLogin

--- NTULAutoLogin.js
+++ NTULAutoLogin.js
@@ -7,12 +7,16 @@
 // @version      0.1
 // @description  auto login
 // @author       eethan1
-// @match        https://web2.cc.ntu.edu.tw/p/s/login2/*
+// @match        https://*.ntu.edu.tw/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    $('form').submit();
-    // Your code here...
+    if(location.href.match(/web2.cc.ntu.edu.tw\/p\/s\/login2/))
+        $('form').submit();
+    else if(location.href.match(/mail.ntu.edu.tw\/owa\/auth\/logon.aspx/))
+        $('.signinbutton').click();
+    else if(location.href.match(/ceiba.ntu.edu.tw\/$/))
+        $('form[name=login2]').submit();
 })();
