0a2a8821760343eb4e6c1dfaa6046715fb1bf5bf
eethan1 <call132call132@gmail.com>
2020-02-07 00:48:48 +0800

    GentleSuit: 5278

---
+++ GentleSuit.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         GentleSuit
+// @namespace    https://github.com/eethan1/TampermonkeyScripts
+// @require http://code.jquery.com/jquery-latest.js
+// @version      0.1
+// @description  Be a gentle.
+// @author       eethan1
+// @match        http://hbo6.hboav.com/v3/player.php*
+// @grant        none
+// ==/UserScript==
+// @updateURL    https://raw.githubusercontent.com/eethan1/TampermonkeyScripts/master/GentleSuit.js
+// @downloadURL  https://raw.githubusercontent.com/eethan1/TampermonkeyScripts/master/GentleSuit.js
+(function() {
+    'user strict';
+    console.log('match!');
+    if(location.href.match(/hbo6\.hboav\.com\/v3\/player\.php/))
+        $('#inplayer').remove();
+})();
