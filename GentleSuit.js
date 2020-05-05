7ac201fa6ff3ee29e9077eb3151bd0e53b21a322
eethan1 <call132call132@gmail.com>
2020-05-05 21:51:47 +0800

    upgrade

--- GentleSuit.js
+++ GentleSuit.js
@@ -2,10 +2,10 @@
 // @name         GentleSuit
 // @namespace    https://github.com/eethan1/TampermonkeyScripts
 // @require http://code.jquery.com/jquery-latest.js
-// @version      0.1
+// @version      0.1.2
 // @description  Be a gentle.
 // @author       eethan1
-// @match        http://hbo6.hboav.com/v3/player.php*
+// @match        http*://hbo6.hboav.com/v3/player.php*
 // @grant        none
 // ==/UserScript==
 // @updateURL    https://raw.githubusercontent.com/eethan1/TampermonkeyScripts/master/GentleSuit.js
--- NTUAutoLogin.js
+++ NTUAutoLogin.js
@@ -3,7 +3,7 @@
 // @namespace    https://github.com/eethan1/TampermonkeyScripts
 // @updateURL    https://raw.githubusercontent.com/eethan1/TampermonkeyScripts/master/NTUAutoLogin.js
 // @downloadURL  https://raw.githubusercontent.com/eethan1/TampermonkeyScripts/master/NTUAutoLogin.js
-// @version      0.5.1
+// @version      0.5.2
 // @description  auto login
 // @author       eethan1
 // @match        https://*.ntu.edu.tw/*
@@ -16,13 +16,13 @@
         document.querySelector('form').submit();
     else if(location.href.match(/mail.ntu.edu.tw\/owa\/auth\/logon.aspx/))
         document.querySelector('.signinbutton').click();
-    else if(location.href.match(/ceiba.ntu.edu.tw\/$/))
+    else if(location.href.match(/ceiba.ntu.edu.tw\/(index\.php)+$/))
         document.querySelector('form[name=login2]').submit();
-    else if(location.href.match(/https:\/\/ceiba.ntu.edu.tw\/login_test.php/))
+    else if(location.href.match(/ceiba.ntu.edu.tw\/login_test.php/))
         document.querySelector('form[name=login2]').submit();
-    else if(location.href.match(/https:\/\/adfs.ntu.edu.tw\/adfs\/ls\//)){
+    else if(location.href.match(/adfs.ntu.edu.tw\/adfs\/ls\//)){
         document.querySelector('#ContentPlaceHolder1_SubmitButton').click();
-    }else if(location.href.match(/https:\/\/cool.ntu.edu.tw\/login\/portal/)){
+    }else if(location.href.match(/cool.ntu.edu.tw\/login\/portal/)){
         saml.click();
     }
 })();
