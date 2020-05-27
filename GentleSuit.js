d8f9c920a169d6397b73be1ceb5aa2fc9173bfab
eethan1 <call132call132@gmail.com>
2020-05-27 20:05:27 +0800

    m3u8 file

--- GentleSuit.js
+++ GentleSuit.js
@@ -2,17 +2,24 @@
 // @name         GentleSuit
 // @namespace    https://github.com/eethan1/TampermonkeyScripts
 // @require http://code.jquery.com/jquery-latest.js
-// @version      0.1.2
+// @version      0.1.3
 // @description  Be a gentle.
 // @author       eethan1
-// @match        http*://hbo6.hboav.com/v3/player.php*
+// @match        http*://hbo6.hboav.com/*/player.php*
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/eethan1/TampermonkeyScripts/master/GentleSuit.js
 // @downloadURL  https://raw.githubusercontent.com/eethan1/TampermonkeyScripts/master/GentleSuit.js
+// @run-at document-end
 // ==/UserScript==
 (function() {
     'user strict';
     console.log('match!');
-    if(location.href.match(/hbo6\.hboav\.com\/v3\/player\.php/))
-        $('#inplayer').remove();
+    console.log('remove inpley');
+    $('#inplayer').remove();
+    let m3u8url = document.body.innerHTML.match(/'http.*m3u8.*'/)[0].slice(1,-1);
+    let a = document.createElement('a');
+    a.href = m3u8url;
+    a.innerText = 'm3u8';
+    document.body.insertBefore(a,document.body.childNodes[2])
+    console.log(m3u8url);
 })();
