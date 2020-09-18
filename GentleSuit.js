701c5ce780fa7775cf93593022c3475a00b1bec5
eethan1 <call132call132@gmail.com>
2020-09-18 21:54:31 +0800

    v0.1.7 upgrade

--- GentleSuit.js
+++ GentleSuit.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         GentleSuit
 // @namespace    https://github.com/eethan1/TampermonkeyScripts
-// @version      0.1.6
+// @version      0.1.7
 // @description  Be a gentle.
 // @author       eethan1
-// @match        http*://hbo6.hboav.com/*/player.php*
+// @match        http*://hbo6.hboav.com/*/*/Player.php*
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/eethan1/TampermonkeyScripts/master/GentleSuit.js
 // @downloadURL  https://raw.githubusercontent.com/eethan1/TampermonkeyScripts/master/GentleSuit.js
@@ -15,6 +15,8 @@
     let m3u8url = document.body.innerHTML.match(/'http.*m3u8.*'/)[0].slice(1,-1);
     console.log('match!');
     let c = document.getElementById('inplayer');
+    document.body.removeChild(document.querySelector('.cc5278_banner_ad'));
+    document.body.removeChild(document.querySelector('#warming'));
     c.parentNode.removeChild(c);
     console.log('remove inpley');
 
